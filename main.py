6f42bfaa2bf697926563d9375a9fa54e21b44f10
afonsopaulo869 <afonsopaulo869@gmail.com>
2026-08-08 14:16:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from groq import Groq
+import streamlit as st
+
+
+
+# pip install groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+st.title("Conversa com o Piloto")
+pergunta  = st.text_input('pergunta:')
+if st.button('enviar'):
+    # if pergunta.strip():
+        reposta =  client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        # temperature=0.7,
+
+        messages=[
+        {
+        'role':'system',
+        'content':"Você é um piloto de avião."
+        },
+        {
+            'role':'user',
+            'content': pergunta
+           
+        }
+        ]
+        )
+
+        st.text(reposta.choices[0].message.content)
+
